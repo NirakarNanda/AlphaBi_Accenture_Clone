7bb9125bacf08ee0ba3d3aaa2bc4838ff2692856
Nirakar Nanda <115453242+NirakarNanda@users.noreply.github.com>
2023-11-12 04:59:49 +0530

    added hero section to the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import Image from 'next/image'
+import HeroSection from "./components/HeroSection";
 
 export default function Home() {
   return (
     <main className="flex mr-4 min-h-screen flex-col bg-[#121212]">
+        <div className="container mt-24 mx-auto px-12 py-4">
+        <HeroSection />
+      </div>
     </main>
   )
 }
